5d92d87ef5b2eb0c35ac9fe4326e63ee0a910823
Михайло Капітанов <Mykhailo.Kapitanov@infiz.khpi.edu.ua>
2024-04-06 13:46:34 +0300

    Добавьте файлы проекта.

---
+++ lab11_kapitanov_var10_1/lab11_kapitanov_var10_1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "ukr");
+    char a, b;
+    cout << "Введiть значення для змiнної a: ";
+    cin >> a;
+    cout << "Введiть значення для змiнної b: ";
+    cin >> b;
+    cout << "Перед обмiном: a = " << a << ", b = " << b << endl;
+    char* pointerA = &a, * pointerB = &b;
+    char temp = *pointerA;
+    *pointerA = *pointerB;
+    *pointerB = temp;
+    cout << "Пiсля обмiну: a = " << a << ", b = " << b << endl;
+    return 0;
+}
